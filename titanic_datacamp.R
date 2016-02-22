534374d9dc5b7ce2b027ec8e63ef971cffde6471
dcamposliz <dcampos.liz@gmail.com>
2016-02-21 16:05:26 -0800

    finished the titanic / kaggle tutorial

--- titanic_datacamp.R
+++ titanic_datacamp.R
@@ -339,14 +339,90 @@
 
 ####
 ####
-###### - 
+###### - What is a Random Forest
 ####
 ####
 
+	# this sample code will help me clean my dataset from missing data
+	# it will also help me 
+
+	# All data, both training and test set
+	all_data
+
+	# the Random Forest technique handles the overfitting problem you 
+	# faced with decision trees. It grows multiple (very deep) classification 
+	# trees using the training set. At the time of prediction, each tree is used 
+	# to come up with a prediction and every outcome is counted as a vote. 
+	# For example, if you have trained 3 trees with 2 saying a passenger in the 
+	# test set will survive and 1 says he will not, the passenger will be 
+	# classified as a survivor. This approach of overtraining trees, but having 
+	# the majority's vote count as the actual classification decision, 
+	# avoids overfitting.
+
+	# Passenger on row 62 and 830 do not have a value for embarkment. 
+	# Since many passengers embarked at Southampton, we give them the value S.
+	all_data$Embarked[c(62, 830)] <- "S"
+
+	# Factorize embarkment codes.
+	# what does it mean to factorize data? okay, factors are fucking chill.
+	# check out this link: http://www.stat.berkeley.edu/~s133/factors.html
+	all_data$Embarked <- factor(all_data$Embarked)
+	# we're basically bucketizing stuff,
+	# and btw, you learned this in intro to R, idiot
+
+	# Passenger on row 1044 has an NA Fare value. Let's replace it with the median fare value.
+	all_data$Fare[1044] <- median(all_data$Fare, na.rm = TRUE)
+	# why the median and not the average?
+
+	# How to fill in missing Age values?
+	# We make a prediction of a passengers Age using the other variables and a decision tree model. 
+	# This time you give method = "anova" since you are predicting a continuous variable.
+	library(rpart)
+	predicted_age <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title + family_size,
+	                       data = all_data[!is.na(all_data$Age),], method = "anova")
+	all_data$Age[is.na(all_data$Age)] <- predict(predicted_age, all_data[is.na(all_data$Age),])
+	# apparently "anova" does continuous variable things
 
 
+	# Split the data back into a train set and a test set
+	train <- all_data[1:891,]
+	test <- all_data[892:1309,]
+
+	# we are basically 'intelligently' filling out missing values, and wrangling the shit out of data
+
 ####
 ####
-###### - 
+###### - A Random Forest analysis in R
 ####
 ####
+
+	# train and test are available in the workspace
+	str(train)
+	str(test)
+
+	# Load in the package
+	library(randomForest)
+
+	# Train set and test set
+	str(train)
+	str(test)
+
+	# Set seed for reproducibility
+	set.seed(111)
+
+	# Apply the Random Forest Algorithm
+	my_forest <- randomForest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title, data = train, importance = TRUE, ntree = 1000)
+
+	# Make your prediction using the test set
+	my_prediction <- predict(my_forest, test)
+	my_prediction
+
+	# Create a data frame with two columns: PassengerId & Survived. Survived contains your predictions
+	my_solution <- data.frame(PassengerId = test$PassengerId, Survived = my_prediction)
+	my_solution
+
+	# Write your solution away to a csv file with the name my_solution.csv
+	write.csv(my_solution, file = "my_solution.csv", row.names = FALSE)
+
+
+# DONE WITH THIS, MOTHERFUCKER !!
